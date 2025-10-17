bb6647607ec17601c3d6957b784a6885be637ca3
pikachuim <pikachuim@qq.com>
2025-10-17 16:03:50 +0800

    add(cloud): aliyun esa

--- node-functions/[[index]].ts
+++ node-functions/[[index]].ts
@@ -1,34 +1,42 @@
 import {Context, Hono} from 'hono'
 import {cors} from "hono/cors";
-import {updated} from "../src/share/zones";
-
+import {Zones} from "../src/share/zones";
 
 export const app = new Hono()
 
-// API-更新域名回源信息 =========================================================
+// API-更域名回源信息 =========================================================
 app.use('/api/:cloud/zones/update', async (c: Context): Promise<any> => {
     const cloud: string = c.req.param('cloud')
-    return await updated(c, cloud)
+    const zones: Zones = new Zones(c, cloud)
+    await zones.initial()
+    return await zones.updated()
 })
 
 // API-创建域名回源信息 =========================================================
 app.use('/api/:cloud/zones/create', async (c: Context): Promise<any> => {
     const cloud: string = c.req.param('cloud')
-    return await updated(c, cloud)
+    const zones: Zones = new Zones(c, cloud)
+    await zones.initial()
+    return await zones.created()
 })
 
 // API-删除域名回源信息 =========================================================
 app.use('/api/:cloud/zones/delete', async (c: Context): Promise<any> => {
     const cloud: string = c.req.param('cloud')
-    return await updated(c, cloud)
+    const zones: Zones = new Zones(c, cloud)
+    await zones.initial()
+    return await zones.deleted()
 })
 
 // API-获取域名回源信息 =========================================================
 app.use('/api/:cloud/zones/source', async (c: Context): Promise<any> => {
     const cloud: string = c.req.param('cloud')
-    return await updated(c, cloud)
+    const zones: Zones = new Zones(c, cloud)
+    await zones.initial()
+    return await zones.sourced()
 })
 
-// 跨域处理 =====================================================================
+
+// 跨域处理 ===================================================================
 app.use('/*', cors());
 export default app
